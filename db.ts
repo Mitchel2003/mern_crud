4689e2abf9c8ea02b80aa9a5f2fbe2372d4627be
Michael Antony <avilesmaicol.08@gmail.com>
2024-07-23 14:27:24 -0500

    update

--- db.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectionDB = async () => {
-  try {
-    await mongoose.connect('mongodb+srv://avilesmaicol08:123.maicol@mern-crud-db.rnks6cx.mongodb.net/?retryWrites=true&w=majority&appName=mern-crud-db');
-    console.log("connection sucessful");
-  } catch (e) { console.log("Error to try conect with database: " + e) }
-}
