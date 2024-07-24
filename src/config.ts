c3fecd2f935768942e6b459702c495806b254ba7
Michael Antony <avilesmaicol.08@gmail.com>
2024-07-24 13:31:50 -0500

    update

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mern-crud-db";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
+export const PORT = process.env.PORT || 4000;
