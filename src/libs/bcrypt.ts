cfeba31486389683fa558072470688da1423a8d7
Michael Antony <avilesmaicol.08@gmail.com>
2024-07-25 20:04:37 -0500

    update

--- src/libs/bcrypt.ts
+++ src/libs/bcrypt.ts
@@ -1,1 +1,20 @@
+import { hash, compare } from "bcryptjs";
 
+/**
+ *
+ *
+ *
+ */
+export const encrypt = async (pass: string, salt: number): Promise<string> => {
+  return await hash(pass, salt);
+}
+
+/**
+ *
+ *
+ *
+ *
+ */
+export const verified = async (pass: string, passHash: string = 'false'): Promise<boolean> => {
+  return await compare(pass, passHash);
+}
