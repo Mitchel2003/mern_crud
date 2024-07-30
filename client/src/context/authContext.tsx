0f64ed7109aee4e2b5388f1f6fde8bdde458556a
Michael Antony <avilesmaicol.08@gmail.com>
2024-07-30 18:57:07 -0500

    update

--- client/src/context/authContext.tsx
+++ client/src/context/authContext.tsx
@@ -1,22 +1,22 @@
 import { createContext, useContext, useState } from 'react';
 import { loginRequest, registerRequest } from '../api/auth';
 
-export const AuthContext = createContext();
+const AuthContext = createContext();
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
-  if(!context) throw new Error('Error to use context');
+  if(!context) throw new Error('Error to try use context');
   return context;
 }
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
 
   const signin = async (user) => await loginRequest(user);
   const signup = async (user) => await registerRequest(user);
 
   return (
-    <AuthContext.Provider value={{user, signin, signup}}>
+    <AuthContext.Provider value={{ user, signin, signup }}>
       { children }
     </AuthContext.Provider>
   )
