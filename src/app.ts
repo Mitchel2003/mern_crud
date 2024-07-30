588968e9ef635b2b20f723b176dc9e843f1ffd20
Michael Antony <avilesmaicol.08@gmail.com>
2024-07-30 13:15:58 -0500

    update

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,10 @@ import authRoutes from "./routes/auth.routes";
 import taskRoutes from "./routes/task.routes";
 
 const app = express();
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
 
 app.use('/api', authRoutes);
 app.use('/api', taskRoutes);
