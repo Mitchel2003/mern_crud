b15e19b27c8cc0b111b82ecc547611092ad09b94
Michael Antony <avilesmaicol.08@gmail.com>
2024-07-30 13:07:57 -0500

    update

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 const path = 'http://localhost:4000/api';
 
-export const loginRequest = async (req: object) => { axios.post(`${path}/login`, req) }
-export const registerRequest = async (data: object) => { axios.post(`${path}/register`, data) }
+export const loginRequest = async (user: object) => axios.post(`${path}/login`, user);
+export const registerRequest = async (user: object) => axios.post(`${path}/register`, user);
