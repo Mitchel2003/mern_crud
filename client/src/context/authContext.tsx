abb66847b7a5495e89fe608c5d5eb3686d9fed57
Michael Antony <avilesmaicol.08@gmail.com>
2024-07-30 15:23:07 -0500

    update

--- client/src/context/authContext.tsx
+++ client/src/context/authContext.tsx
@@ -1,1 +1,23 @@
+import { createContext, useState } from 'react';
+import { loginRequest, registerRequest } from '../api/auth';
 
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+
+  const signup = async (user) => {
+    const req = await registerRequest(user);
+    console.log(req);
+  }
+  const signin = async (user) => {
+    const req = await loginRequest(user);
+    console.log(req);
+  }
+
+  return (
+    <AuthContext.Provider value={{user, signup, signin}}>
+      { children }
+    </AuthContext.Provider>
+  )
+}
