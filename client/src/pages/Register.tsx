ee6f90777994b70c77401796106ec8c15b11c789
Michael Antony <avilesmaicol.08@gmail.com>
2024-08-13 13:37:04 -0500

    update

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,11 +1,11 @@
-                  import { useEffect } from "react";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext"
-import { useForm } from "react-hook-form";
 
 function Register() {
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const { signup, isAuth, err: registerErrors } = useAuth();
+  const { signup, isAuth, errors: errs } = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => { if (isAuth) return navigate("/tasks") }, [isAuth]);
@@ -17,7 +17,7 @@ function Register() {
       <div>Register</div>
       <form onSubmit={onSubmit} >
 
-        { registerErrors.map((e, index) => (<div className="bg-red-500 text-white" key={index}> {e} </div>)) }
+        { errs.map((e, index) => (<div className="bg-red-500 text-white" key={index}> {e} </div>)) }
         
         <input type="text" placeholder="Username" autoFocus {...register('username', { required: true })} />
         {errors.username && (<p className="text-red-500">Username is required</p>)}
