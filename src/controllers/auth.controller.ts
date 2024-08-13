53a0d1ade543e4bc26ddfd379156fc2f412e695f
Michael Antony <avilesmaicol.08@gmail.com>
2024-08-13 13:26:11 -0500

    update

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,14 +5,14 @@ import ExtendsRequest from "../interfaces/request.interface";
 import { encrypt, verified } from "../libs/bcrypt.handle";
 import { generateAccessToken } from "../libs/jwt.handle";
 import User from "../models/user.model";
-/*--------------------------------------------------controllers--------------------------------------------------*/
+
 export const login = async (req: Request, res: Response) => {
   try { 
     const user = await verifyCredencials(req, res);
     const token = await generateAccessToken({ id: user._id });
     res.cookie("token", token);
     res.json({ id: user._id });
-  } catch (e) { res.status(500).json(["Error to login => " + e ]) }
+  } catch (e) { res.status(500).json(["Error to try login"]) }
 }
 
 export const register = async (req: Request, res: Response) => {
@@ -22,7 +22,7 @@ export const register = async (req: Request, res: Response) => {
     const token = await generateAccessToken({ id: user._id });
     res.cookie("token", token);
     res.send("User registed");
-  } catch (e) { res.status(500).json(["Error to register => " + e ]) }
+  } catch (e) { res.status(500).json(["Error to try register"]) }
 }
 
 export const logout = (req: Request, res: Response) => {
@@ -48,12 +48,12 @@ async function verifyCredentials({ body }: Request, res: Response): Promise<Docu
   if (!isMatch || !userFound) res.status(403);
   return userFound as Document;
 }
-async function isAccountFound(req: Request, res: Response): Promise<Response> {
+async function isAccountFound(req: Request, res: Response) {
   const { email } = req.body;
   const userFound = await User.findOne({ email });
   if(userFound) return res.status(403);
 }
-async function createUserEncrypt(req: Request): Promise<Document> {
+async function createUserEncrypt(req: Request) {
   const { username, email, password } = req.body;
   const passHash = await encrypt(password, 10);
   const user = new User({ username, email, password: passHash });
