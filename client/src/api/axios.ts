6f33dbf9ab9d46cbf88a83978ba5bec8c0371ae2
mitchel2003 <avilesmaicol.08@gmail.com>
2024-09-01 22:31:18 -0500

    update

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const instance = axios.create({ // baseURL: 'http://localhost:4000/api',
+const instance = axios.create({
+    //baseURL: 'http://localhost:4000/api',
     baseURL: 'https://rest-api-app-react.onrender.com/api',
     withCredentials: true
 })
--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 import "dotenv/config"
 
+const MONGODB_URI = process.env.MONGODB_URI as string;
 export const connectionDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI ?? '');
+    await mongoose.connect(MONGODB_URI);
     console.log("connection sucessful");
   } catch (e) { console.log("Error to try conect with database: " + e) }
 }
