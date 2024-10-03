2d18a9880c0ffa4f249224e27c45e00e7029cc81
mitchel2003 <avilesmaicol.08@gmail.com>
2024-10-03 08:31:01 -0500

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from "path"
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,8 +9,8 @@ export default defineConfig({
   /* shadcn/ui */
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "#": path.resolve(__dirname, "./src/components")
+      '@': path.resolve(__dirname, './src'),
+      '#': path.resolve(__dirname, './src/components'),
     }
-  },
+  }
 })
