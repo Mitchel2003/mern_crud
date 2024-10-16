660a319f620e61173dfac5f61e542e6bd96b3090
mitchel2003 <avilesmaicol.08@gmail.com>
2024-10-16 08:18:59 -0500

    update => remove bg animation (framer-motion)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom"
 import { useEffect } from "react"
 
 import LoginSection from "@/sections/login"
-import FloatingShape from "@/components/others/FloatingShape"
 
 const Login = () => {
   const { theme } = useThemeContext()
@@ -16,15 +15,9 @@ const Login = () => {
   }, [isAuth, navigate])
 
   return (
-    <>
-      <div className="w-full max-w-md relative">
-        <FloatingShape color="bg-green-500" size="w-64 h-64" top="-5%" left="10%" delay={0} />
-        <FloatingShape color="bg-emerald-500" size="w-48 h-48" top="-20%" left="80%" delay={5} />
-        <FloatingShape color="bg-lime-500" size="w-32 h-32" top="40%" left="-10%" delay={2} />
-
-        <LoginSection theme={theme} />
-      </div>
-    </>
+    <div className="w-full max-w-md relative">
+      <LoginSection theme={theme} />
+    </div>
   )
 }
 
