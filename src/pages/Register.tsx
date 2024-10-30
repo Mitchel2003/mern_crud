cbb54084d89bf06321fd12c2d29564af41530fc6
mitchel2003 <avilesmaicol.08@gmail.com>
2024-10-30 09:28:27 -0500

    Caution => fix issues to deploy

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,12 +1,11 @@
-import { useThemeContext } from "@/context/ThemeContext"
+// import { useThemeContext } from "@/context/ThemeContext"
 import { useAuthContext } from "@/context/AuthContext"
 import { useNavigate } from "react-router-dom"
 import { useEffect } from "react"
-
 // import RegisterSection from "@/sections/register"
 
 const Register = () => {
-  const { theme } = useThemeContext()
+  // const { theme } = useThemeContext()
   const { isAuth } = useAuthContext()
   const navigate = useNavigate()
 
