df96c4bd4debc7e05bd8ff8acb518d588b8a2194
mitchel2003 <avilesmaicol.08@gmail.com>
2024-11-12 14:35:49 -0500

    update => fix routes

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,8 +1,8 @@
 import axios from "./axios";
 
-export const loginRequest = async (user: object) => axios.post('/login', user);
-export const registerRequest = async (user: object) => axios.post('/register', user);
-export const verifyAuthRequest = async () => axios.get('/verify-auth');
+export const loginRequest = async (user: object) => axios.post('/auth/login', user);
+export const registerRequest = async (user: object) => axios.post('/auth/register', user);
+export const verifyAuthRequest = async () => axios.get('/auth/verify-auth');
 
 //verify action
 export const verifyActionRequest = async (mode: string, body: object) => axios.post(`/auth/verify-action/${mode}`, body);
--- src/api/curriculum.ts
+++ src/api/curriculum.ts
@@ -1,7 +1,7 @@
 import axios from "./axios"
 
-export const getCVRequest = async (id: string) => axios.get(`/cv/${id}`)
-export const getCVsRequest = async () => axios.get('/cvs')
-export const createCVRequest = async (curriculum: object) => axios.post('/cv', curriculum)
-export const updateCVRequest = async (id: string, curriculum: object) => axios.put(`/cv/${id}`, curriculum)
-export const deleteCVRequest = async (id: string) => axios.delete(`/cv/${id}`)
+export const getCVRequest = async (id: string) => axios.get(`/form/cv/${id}`)
+export const getCVsRequest = async () => axios.get('/form/cvs')
+export const createCVRequest = async (curriculum: object) => axios.post('/form/cv', curriculum)
+export const updateCVRequest = async (id: string, curriculum: object) => axios.put(`/form/cv/${id}`, curriculum)
+export const deleteCVRequest = async (id: string) => axios.delete(`/form/cv/${id}`)
