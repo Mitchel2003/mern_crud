8d24d52a1304c0c2a7da884168c5adba002e41dc
mitchel2003 <avilesmaicol.08@gmail.com>
2024-11-25 09:40:36 -0500

    update => fix image CardEquipment

--- src/components/pages/dashboard/CardEquipment.tsx
+++ src/components/pages/dashboard/CardEquipment.tsx
@@ -1,25 +1,23 @@
 import { Card, CardContent, CardHeader, CardTitle } from '#/ui/card'
 import { Equipment } from '@/types/dashboard.type'
 import { Badge } from '#/ui/badge'
-import React from 'react'
 
 interface CardEquipmentProps { equipment: Equipment }
-
-const CardEquipment: React.FC<CardEquipmentProps> = ({ equipment }) => {
+const CardEquipment = ({ equipment }: CardEquipmentProps) => {
   return (
     <Card className="h-full">
       <CardHeader className="pb-2">
         <CardTitle className="text-lg">{equipment.name}</CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="aspect-square relative mb-4">
+        <div className="relative mb-4">
           <img
             src={equipment.imageUrl}
             alt={equipment.name}
-            className="object-cover rounded-md"
+            className="w-full h-[200px] object-cover rounded-md"
           />
         </div>
-        <p className="text-sm mb-2">{equipment.status}</p>
+        <p className="text-lg mb-2">{equipment.status}</p>
         <Badge variant="secondary">
           Próximo mantenimiento: {equipment.nextMaintenance.toLocaleDateString()}
         </Badge>
