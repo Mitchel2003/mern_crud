64363b42c25a40d19bde2a0ec957a04f2d913fe9
mitchel2003 <avilesmaicol.08@gmail.com>
2025-01-03 04:38:22 +0100

    UPDATE => fix schema - remember that references are uid not editables

--- src/schemas/location.schema.ts
+++ src/schemas/location.schema.ts
@@ -7,12 +7,8 @@ export const headquarterSchema = z.object({
   address: z.string()
     .min(3, "La dirección debe tener al menos 3 caracteres")
     .max(100, "La dirección debe tener menos de 100 caracteres"),
-  client: z.string()
-    .min(3, "El cliente debe tener al menos 3 caracteres")
-    .max(50, "El cliente debe tener menos de 50 caracteres"),
-  city: z.string()
-    .min(3, "La ciudad debe tener al menos 3 caracteres")
-    .max(50, "La ciudad debe tener menos de 50 caracteres"),
+  client: z.string({ required_error: "El cliente es requerido" }),
+  city: z.string({ required_error: "La ciudad es requerida" })
 })
 
 export const citySchema = z.object({
@@ -20,10 +16,7 @@ export const citySchema = z.object({
     .string()
     .min(3, "El nombre debe tener al menos 3 caracteres")
     .max(50, "El nombre debe tener menos de 50 caracteres"),
-  state: z
-    .string()
-    .min(3, "El estado debe tener al menos 3 caracteres")
-    .max(50, "El estado debe tener menos de 50 caracteres"),
+  state: z.string({ required_error: "El estado es requerido" })
 })
 
 export const stateSchema = z.object({
@@ -31,10 +24,7 @@ export const stateSchema = z.object({
     .string()
     .min(3, "El nombre debe tener al menos 3 caracteres")
     .max(50, "El nombre debe tener menos de 50 caracteres"),
-  country: z
-    .string()
-    .min(3, "El país debe tener al menos 3 caracteres")
-    .max(50, "El país debe tener menos de 50 caracteres"),
+  country: z.string({ required_error: "El país es requerido" })
 })
 
 export const countrySchema = z.object({
