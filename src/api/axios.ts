e15d55580371c64aeed6a1f464fbf621a279ac5b
mitchel2003 <avilesmaicol.08@gmail.com>
2025-03-04 16:17:01 +0100

    [FIX => route api]

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const instance = axios.create({
-  // baseURL: 'https://rest-api-qvo9.onrender.com/api', /* to mode production */
-  baseURL: 'http://localhost:4000/api', /* to mode development */
+  baseURL: 'https://rest-api-qvo9.onrender.com/api', /* to mode production */
+  // baseURL: 'http://localhost:4000/api', /* to mode development */
   withCredentials: true
 })
 
