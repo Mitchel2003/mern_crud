7debe21261bbd36ab342c40fd697952893de9fe4
mitchel2003 <avilesmaicol.08@gmail.com>
2025-03-09 01:50:18 +0100

    [TRY => remove rollupOptions]

--- vite.config.ts
+++ vite.config.ts
@@ -12,24 +12,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       '#': path.resolve(__dirname, './src/components')
     }
-  },
-
-  /**
-   * This configures the build process to split the code into smaller chunks.
-   * This is done to improve the performance of the application.
-   */
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            if (id.includes('@mui')) return 'mui';
-            if (id.includes('recharts')) return 'charts';
-            if (id.includes('@react-pdf')) return 'pdf';
-            return 'vendor';
-          }
-        }
-      }
-    }
   }
 })
