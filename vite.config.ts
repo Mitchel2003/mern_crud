48c5b698abb9664430a77c26f9a5fbef3033f778
mitchel2003 <avilesmaicol.08@gmail.com>
2025-03-09 01:47:25 +0100

    [TRY => remove terser and fix rollupOptions]

--- vite.config.ts
+++ vite.config.ts
@@ -19,8 +19,17 @@ export default defineConfig({
    * This is done to improve the performance of the application.
    */
   build: {
-    minify: 'terser',
-    terserOptions: { compress: { drop_console: true, drop_debugger: true } },
-    rollupOptions: { output: { manualChunks(id) { if (id.includes('node_modules')) return id.split('node_modules/')[1].split('/')[0] } } }
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            if (id.includes('@mui')) return 'mui';
+            if (id.includes('recharts')) return 'charts';
+            if (id.includes('@react-pdf')) return 'pdf';
+            return 'vendor';
+          }
+        }
+      }
+    }
   }
 })
