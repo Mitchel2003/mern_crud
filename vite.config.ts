917751e0ca8f8a11008ffb17a4e1bb6a2efb807c
mitchel2003 <avilesmaicol.08@gmail.com>
2025-03-09 01:32:15 +0100

    [TRY => optimize app build]

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,15 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       '#': path.resolve(__dirname, './src/components')
     }
+  },
+
+  /**
+   * This configures the build process to split the code into smaller chunks.
+   * This is done to improve the performance of the application.
+   */
+  build: {
+    minify: 'terser',
+    terserOptions: { compress: { drop_console: true, drop_debugger: true } },
+    rollupOptions: { output: { manualChunks(id) { if (id.includes('node_modules')) return id.split('node_modules/')[1].split('/')[0] } } }
   }
 })
